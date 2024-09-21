ba95c3a0a4b6e84e7d68b5e7cb5eb53c060f3be1
SUBHARJUN <158795613+arjunnxd@users.noreply.github.com>
2024-09-21 19:06:08 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,9 @@
+import pywhatkit as w
+import time
+import pyautogui
+import keyboard as k
+w.sendwhatmsg("+91 8019179250", 'happy birthday', 2, 54)
+pyautogui.click(1050, 950)
+time.sleep(2)
+k.press_and_release('enter')
+
